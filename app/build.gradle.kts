ef2232f182df2766367b49f5dc3847c69d8ef8d3
[Camila] <cagv.cg@gmail.com>
2024-11-13 21:46:27 -0400

    Ejercicio 8 oficial

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "com.bootcamp.ejercicioochokotlin"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.bootcamp.ejercicioochokotlin"
